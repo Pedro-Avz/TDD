d64ae34f3d850db487c58bb2ce8831dc8383ee33
Pedro-Avz <pedroalvarez96@hotmail.com>
2024-04-13 10:54:26 -0300

    test: adicionar teste para verificar equilibrio do trenó

--- src/test/java/org/example/test/TestTreno.java
+++ src/test/java/org/example/test/TestTreno.java
@@ -6,6 +6,21 @@ import static org.junit.Assert.assertEquals;
 
 public class TestTreno {
 
+    @Test
+    public void testEquilibrioS() {
+        int[] presentes = {4, 6, 2};
+        String resultado = Treno.equilibrarTreno(presentes);
+        assertEquals("S", resultado);
+        System.out.println("Test Equilibrio 'S' - Papai Noel, o trenó sempre esteve equilibrado.");
+    }
+
+    @Test
+    public void testEquilibrioN() {
+        int[] presentes = {6, 6};
+        String resultado = Treno.equilibrarTreno(presentes);
+        assertEquals("N", resultado);
+        System.out.println("Test Equilibrio 'N' - Papai Noel, o trenó em algum momento não esteve equilibrado.");
+    }
 
 }
 
